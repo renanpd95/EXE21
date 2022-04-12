ef9a06488c04561cfaea2cb0e08e25623ca7e8f2
Renan <88853399+renanpd95@users.noreply.github.com>
2022-04-12 16:25:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+n1 = float(input("Qual a primeira nota? "))
+n2 = float(input("Qual a segunda nota? "))
+n3 = float(input("Qual a terceira nota? "))
+
+result = (n1+n2+n3)/3
+print("Nota do Aluno: ",result)
